97a6d8848af10ccc7fb718a1112bc0ca417ee4b2
facu <facu@facu-PC.fibertel.com.ar>
2023-08-24 23:24:21 -0300

    implement kiss

--- Program.cs
+++ Program.cs
@@ -56,14 +56,11 @@ namespace ToDo
                 string line = Console.ReadLine();
                 // Remove one position
                 int indexToRemove = Convert.ToInt32(line) - 1;
-                if (indexToRemove > -1)
+                if (indexToRemove > -1 && TaskList.Count > 0)
                 {
-                    if (TaskList.Count > 0)
-                    {
-                        string task = TaskList[indexToRemove];
-                        TaskList.RemoveAt(indexToRemove);
-                        Console.WriteLine("Tarea " + task + " eliminada");
-                    }
+                    string task = TaskList[indexToRemove];
+                    TaskList.RemoveAt(indexToRemove);
+                    Console.WriteLine("Tarea " + task + " eliminada");
                 }
             }
             catch (Exception)
@@ -100,10 +97,9 @@ namespace ToDo
         public static void GetTaskList()
         {
                 Console.WriteLine("----------------------------------------");
-                for (int i = 0; i < TaskList.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TaskList[i]);
-                }
+                int indexTaskList = 0;
+                TaskList.ForEach(p=> Console.WriteLine(++indexTaskList + ". " + p));
+
                 Console.WriteLine("----------------------------------------");
         }
     }
